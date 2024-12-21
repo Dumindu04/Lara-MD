9c088ec3f2cd6ccdbcb2c8f1421b16ffc856fa3b
Dumindu04 <166401628+Dumindu04@users.noreply.github.com>
2024-12-21 07:58:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "66xGkIhI#8zXKClf9WHDP8eGi3TEbDQaTmVRuVSjFR3JFkIV1Mb4", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "n3RmBDCT#l4JI9yLQPcRSKFBE0HFj4tOjHIvx-Sf0IsGCGxzm7z0", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",    
